4d1d18491b656889e0b626000248c9bb62eaa8b2
NotBlizzard <furryblizzard@gmail.com>
2015-04-06 10:54:53 -0400

    Make time local.

--- app.js
+++ app.js
@@ -24,44 +24,82 @@ server.listen(port, function() {
 
 io.on('connection', function(socket) {
 
-  socket.on('disconnect', function() {
-    users.pop(socket.username.toLowerCase());
-    io.emit('user left', socket.username);
-  });
+  //When the user first connects.
+  //They will pick a name, then join lobby.
+  //If their name doesn't meet criteria, then the user
+  //will get asked to pick another name.
 
-  socket.on('add user', function(name) {
-    socket.username = name;
-    if (users.indexOf(name.toLowerCase()) > -1) {
+  socket.on('pick username', function(name) {
+    if (name === null || !name || name.length === 0) {
+      return socket.emit('no username');
+    } else if (users.indexOf(name.toLowerCase()) > -1) {
       socket.emit('username taken');
-    } else if (name.length === 0 || !name) {
-      socket.emit('no username');
     }
     users.push(name.toLowerCase());
     if (!socket.rooms.indexOf('lobby') > -1) {
       socket.join('lobby');
     }
-    socket.emit('add user', name);
-    socket.to('lobby').emit('user joined', name);
+    socket.username = name;
+    socket.emit('subscribe', 'lobby');
+    socket.emit('pick username', name);
+    socket.broadcast.to('lobby').emit('user joined room', {
+      name: socket.username,
+      room: 'lobby'
+    });
 
   });
 
+  socket.on('disconnect', function() {
+    try {
+      users.pop(socket.username.toLowerCase());
+    } catch (e) {
+      //the user never picked a name, and disconnected.
+    }
+    for (var room in socket.rooms) {
+      socket.broadcast.to(room).emit('user left room', {
+        user: socket.username,
+        room: room
+      })
+    }
+  });
 
+
+  //When the user joins or leaves a room.
+  //Also if the user switches rooms.
   socket.on('subscribe', function(room) {
     socket.emit('subscribe', room);
     if (socket.rooms.indexOf(room) < -1) {
       socket.join(room);
-      socket.broadcast.to(room).emit('user joined', socket.username);
+      socket.broadcast.to(room).emit('user joined room', {
+        user: socket.username,
+        room: room
+      });
     }
+  });
+
+  socket.on('unsubscribe', function(room) {
+    socket.leave(room);
+    socket.broadcast.to(room).emit('user left room', {
+      user: socket.username,
+      room: room
+    });
   });
 
   socket.on('change room', function(room) {
     socket.emit('change room', room);
   });
 
+  //When the user sends a message.
   socket.on('message', function(data) {
+    if (!socket.username) {
+      socket.emit('no username');
+    }
     data.msg = data.msg.replace(/</g, "&lt;").replace(/>/g, "&gt;");
-    var timestamp = strftime("%H:%M:%S");
-    var colour = md5(socket.username).substr(0,6);
+    var timestamp = strftime("%H:%M:%S", data.timestamp);
+    if (socket.rooms.length == 0) {
+      socket.emit('no rooms');
+    }
+    var colour = md5(socket.username).substr(0, 6);
     io.emit('message', {
       user: socket.username,
       msg: data.msg,
--- js/socket.js
+++ js/socket.js
@@ -2,9 +2,10 @@ var socket = io();
 var roomMessages = {};
 
 $(document).ready(function() {
+
   $(document).on('click', '.room', function(event) {
     $('#room_tabs>div').removeClass('focus');
-    $('#'+event.target.id).addClass('focus');
+    $('#' + event.target.id).addClass('focus');
     socket.emit('change room', event.target.id);
   });
 
@@ -14,46 +15,56 @@ $(document).ready(function() {
     }
   });
 
+  $(document).on('click', '.exit', function(event) {
+    $("#" + event.target.id).remove();
+    socket.emit('unsubscribe', event.target.id);
+  })
+
   $('#send').click(function() {
     var msg = $('#message').val();
     if (msg === '') return false;
+    if ( $("#room_tabs").data('currentroom') === '') {
+      socket.emit('no rooms');
+    }
     if (msg.substr(0, 5) === '/join') {
       var room = msg.split('/join ')[1];
-  $('#message').val('');
-  socket.emit('subscribe', room);
-} else {
-  $('#message').val('');
-  var new_room = $('#room_tabs').data('currentroom');
-  socket.emit('message', {
-    msg: msg,
-    room: new_room
+      $('#message').val('');
+      socket.emit('subscribe', room);
+    } else {
+      $('#message').val('');
+      socket.emit('message', {
+        msg: msg,
+        room: $('#room_tabs').data('currentroom'),
+        timestamp: Date.now
+      });
+    }
   });
-}
-});
 });
 
-socket.on('add user', function(name) {
-  $("#room_tabs").data('currentroom', 'lobby');
-  $('#room_tabs').append('<div class="room focus" id="lobby">lobby</div>');
-  $('#username').val(name);
-});
-
-socket.on('subscribe', function(room) {
-  $('#room_tabs').data('currentroom', room);
-  $('#room_tabs').append('<div class="room" id= "' + room + '">' + room + '</div>');
-});
-
+/* When the user connects and disconnects.
+ */
 socket.on('connect', function() {
-  socket.emit('add user', prompt("What is your username?"));
+  socket.emit('pick username', prompt("What is your username?"));
 });
+
+/* When the user makes their name, check to see if
+ * the name is taken or blank. If not, join lobby.
+ */
+
+socket.on('pick username', function(name) {
+  $("#username").html("<p>"+name+"</p>");
+ })
 
 socket.on('username taken', function() {
-  socket.emit('add user', prompt('The name is already taken. choose another username.'));
+  socket.emit('pick username', prompt('The name is already taken. choose another username.'));
 });
 
 socket.on('no username', function() {
-  socket.emit('add user', prompt('Your username can not be blank. choose another username.'))
+  socket.emit('pick username', prompt('Your username can not be blank. choose another username.'));
 });
+
+/* When the user changes rooms.
+ */
 
 socket.on('change room', function(room) {
   $('#messages').html('');
@@ -63,28 +74,49 @@ socket.on('change room', function(room) {
   } catch (e) {
     msgs = '';
   }
+  $('#room_tabs').data('currentroom', room)
   $('#messages').append(msgs);
 });
 
-socket.on('user joined', function(name) {
-  var msg = "<p id='joinleave'>" + name + " joined</p>";
-  $('#messages').append(msg);
-  roomMessages[room] = roomMessages[room] || [];
-  roomMessages[room].push(msg);
+/* When the user either
+ * joins a room or leaves a room
+ */
+
+socket.on('subscribe', function(room) {
+  $('#room_tabs').data('currentroom', room);
+  if (room === 'lobby') {
+    $('#room_tabs').append('<div class="room" id= "' + room + '">' + room + '</div>');
+  } else {
+    $('#room_tabs').append('<div class="room" id= "' + room + '">' + room + '<i id="'+room+'" class="exit fa fa-times"></i></div>');
+  }
 });
 
-socket.on('user left', function(name) {
-      var msg = "<p id='joinleave'>" + name + " left</p>";
-      $('#messages').append(msg);
-      roomMessages[room] = roomMessages[room] || [];
-      roomMessages[room].push(msg);
+socket.on('unsubscribe', function(room) {
+  if ($("#room_tabs").data('currentroom') === room) {
+    $('#room_tabs').data('currentroom', null);
+  };
+})
+
+socket.on('user joined room', function(data) {
+  var msg = "<p id='joinleave'>" + data.user + " joined</p>";
+  $('#messages').append(msg);
+  roomMessages[data.room] = roomMessages[data.room] || [];
+  roomMessages[data.room].push(msg);
 });
+
+socket.on('user left room', function(data) {
+  var msg = "<p id='joinleave'>" + data.user + " left</p>";
+  $('#messages').append(msg);
+  roomMessages[data.room] = roomMessages[room] || [];
+  roomMessages[data.room].push(msg);
+});
+
+/* When the user attempts to sends a message to a room.
+ */
 
 socket.on('message', function(data) {
-  var room = $('#room_tabs').data('currentroom');
-  var message = '<p>['+data.timestamp+'] <span style="color:#'+data.colour+';"><strong>' +data.user + '</strong></span>: ' + data.msg + '</p>';
+  var message = '<p>[' + data.timestamp + '] <span style="color:#' + data.colour + ';"><strong>' + data.user + '</strong></span>: ' + data.msg + '</p>';
   roomMessages[room] = roomMessages[room] || [];
   roomMessages[room].push(message);
   $('#messages').append(message);
 });
-
